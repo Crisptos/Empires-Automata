c3b40bb6d7905e9f8ec4413a9a53708017354ca5
Christos Mark <baratheoniron@gmail.com>
2023-07-26 20:31:18 -0400

    Options menu skeleton added

--- Scenes/UI/Main Menu/main_menu.gd
+++ Scenes/UI/Main Menu/main_menu.gd
@@ -1,4 +1,10 @@
 extends Control
 
+@onready var options: Control = $CanvasLayer/options_menu
+
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/World/world.tscn")
+
+
+func _on_options_pressed():
+	options.visible = !options.visible
